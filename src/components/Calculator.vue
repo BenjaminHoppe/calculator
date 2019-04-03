2ceebe9612899f67b8947f21a724b1be0e596b81
Benjamin <hoppeconnect@gmail.com>
2019-04-03 17:07:48 -0400

    added all buttons

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,6 +1,26 @@
 <template>
-  <div class="hello">
-    <h1>YOO!</h1>
+  <div class="calculator">
+    <div class="display">bye</div>
+    <div class="btn">C</div>
+    <div class="btn">+/-</div>
+    <div class="btn">%</div>
+    <div class="btn">÷</div>
+    <div class="btn">7</div>
+    <div class="btn">8</div>
+    <div class="btn">9</div>
+    <div class="btn">x</div>
+    <div class="btn">4</div>
+    <div class="btn">5</div>
+    <div class="btn">6</div>
+    <div class="btn">-</div>
+    <div class="btn">1</div>
+    <div class="btn">2</div>
+    <div class="btn">3</div>
+    <div class="btn">+</div>
+    <div class="btn zero">0</div>
+    <div class="btn">.</div>
+    <div class="btn">=</div>
+
 
   </div>
 </template>
@@ -16,21 +36,24 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.calculator {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(50px auto);
+  font-size: 40px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.display {
+  grid-column: 1/5;
+
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.zero {
+  grid-column: 1/3;
 }
-a {
-  color: #42b983;
-}
-h1 {
-  color: red;
+
+.btn {
+  background-color: #eee;
+  border: 1px solid #333;
 }
 </style>
